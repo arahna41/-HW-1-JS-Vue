1c0db82d7b0fa03e1932ad1b75f022de8a3581d2
arahna41 <arahna41@gmail.com>
2021-10-11 12:07:44 +0300

    task2

---
+++ HW_1.2.js
@@ -0,0 +1,15 @@
+// task HW1.2.1
+
+var array = [45, "246", 73, "4", 15, 36, 17, 89, "49", 167, 123];
+var count = 0;
+
+for (var i = 0; i < array.length; i++) {
+  if (typeof array[i] === "number" && array[i] % 3 == 0) {
+    ++count;
+  }
+}
+if (count == 0) {
+  console.log(`В данном массиве нет чисел кратных 3`);
+} else {
+  console.log(`В данном массиве ${count} чисел кратных 3`);
+}
